f26078a3cea89464a8d3c965d03529f703bdbe45
Stanley Stout <stouts@mac.com>
2016-07-22 11:05:42 -0500

    Commit

--- Xcode Version Control/ViewController.swift
+++ Xcode Version Control/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Xcode Version Control
 //
-//  Created by Stanley Stout on 7/22/16.
+//  Created by Chris Stout on 7/22/16.
 //  Copyright © 2016 Chris Stout. All rights reserved.
 //
 
@@ -12,14 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
